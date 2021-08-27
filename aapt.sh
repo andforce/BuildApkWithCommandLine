a03701b13230fc2c6635cc55d4542d75a07bc8b7
diyuan <wangdiyuan1988@gmail.com>
2021-08-27 20:07:46 +0800

    add build.sh

---
+++ aapt.sh
@@ -0,0 +1,24 @@
+export PATH=$PATH:"${ANDROID_HOME}/build-tools/30.0.2/"
+
+SDK_30="${ANDROID_HOME}/platforms/android-23/android.jar"
+echo "$SDK_30"
+
+rm -rf build/
+
+mkdir -p build/{outputs/apk,intermediates/{classes,dex},generated/source}
+
+aapt package -v -f -m -S app/src/main/res -J build/generated/source/ -M app/src/main/AndroidManifest.xml -I "$SDK_30"
+
+javac -verbose -d build/intermediates/classes/ -classpath "$SDK_30" -sourcepath build/generated/source/ app/src/main/java/com/andforce/build/*.java
+
+dx --dex --output=build/intermediates/dex/classes.dex build/intermediates/classes/
+
+aapt package -v -f -M app/src/main/AndroidManifest.xml --min-sdk-version 21 --target-sdk-version 23 -S app/src/main/res -I "$SDK_30" -F build/outputs/apk/unsigned.apk build/intermediates/dex/
+
+jarsigner -verbose -keystore ~/.android/debug.keystore -storepass android -keypass android build/outputs/apk/unsigned.apk androiddebugkey
+
+# rename
+mv build/outputs/apk/unsigned.apk build/outputs/apk/signed.apk
+
+# 安装
+adb install -r -d build/outputs/apk/signed.apk
