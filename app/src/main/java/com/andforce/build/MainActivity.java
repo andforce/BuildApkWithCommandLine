c5059a0359815305c9a1c2db56c60a8ee1b6acfa
diyuan <wangdiyuan1988@gmail.com>
2021-08-27 20:31:21 +0800

    add private value

--- app/src/main/java/com/andforce/build/MainActivity.java
+++ app/src/main/java/com/andforce/build/MainActivity.java
@@ -1,7 +1,9 @@
 package com.andforce.build;
 
 import android.app.Activity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.getResources();
+        int value = Resources.getSystem().getIdentifier("datepicker_view_animator_height", "dimen", "android");
+        int dimensionPixelSize = getResources().getDimensionPixelSize(value);
+        TextView textView = (TextView) findViewById(R.id.first_tv);
+        String text = "@*android:datepicker_view_animator_height, pixelSize: " + dimensionPixelSize;
+        textView.setText(text);
     }
 }
